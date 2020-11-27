216ed56fcb4e46293d5dc965034138bb0b8be0f6
JAY TAILOR <tailorj64@gmail.com>
2020-11-28 01:32:05 +0530

    Serving simple html from the server

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<html>`);
+    res.write(`<head><title>First page</title></head>`);
+    res.write(`<body><h2>Hello form the node js server</h2></body>`);
+    res.write(`</html>`);
+    res.end();
 });
 
 server.listen(PORT);

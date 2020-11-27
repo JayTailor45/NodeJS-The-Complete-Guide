823b94288133fa6da81086e56ad9680d496ded50
JAY TAILOR <tailorj64@gmail.com>
2020-11-28 02:27:08 +0530

    Refactor the code with module export

--- app.js
+++ app.js
@@ -1,40 +1,8 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write(`<html>`);
-        res.write(`<head><title>First page</title></head>`);
-        res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`);
-        res.write(`</html>`);
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write(`<html>`);
-    res.write(`<head><title>First page</title></head>`);
-    res.write(`<body><h2>Hello form the node js server</h2></body>`);
-    res.write(`</html>`);
-    res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(PORT);

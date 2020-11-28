6a66c569788c3ae1450e07296b80036c3a6f6ade
JAY TAILOR <tailorj64@gmail.com>
2020-11-29 01:55:42 +0530

    Setup express routes and serve html

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const http = require('http');
-const routes = require('./routes');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const PORT = 3000;
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(PORT);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use('/', (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(PORT);

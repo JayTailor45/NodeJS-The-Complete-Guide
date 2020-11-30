4f2aedaf68a8db0a37e9b3b1c8b06376b2fd2e0f
JAY TAILOR <tailorj64@gmail.com>
2020-12-01 00:25:26 +0530

    Refactore code to use MVC pattern

--- app.js
+++ app.js
@@ -8,17 +8,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorsController = require('./controllers/errors');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''});
-});
+app.use(errorsController.getNotFound);
 
 app.listen(3000);

eaab725b359d426ca7d694f5ee0782e12c37ef73
JAY TAILOR <tailorj64@gmail.com>
2020-12-01 00:58:08 +0530

    Refactor model

--- models/product.js
+++ models/product.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 const path = require('path');
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
 
-const products = [];
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, ((err, fileContent) => {
+        if (err) {
+            cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    }));
+};
 
 module.exports = class Product {
     constructor(title) {
@@ -9,28 +17,16 @@ module.exports = class Product {
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
         });
-        products.push(this.title);
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, ((err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        }));
+        getProductsFromFile(cb);
     }
 
 };
